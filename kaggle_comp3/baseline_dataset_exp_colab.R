e2dfd12e3bced344649388562fc2a4187fe2a434
mpirola <malenapirola@gmail.com>
2023-11-03 21:53:16 +0000

    solo los meses necesarios

--- kaggle_comp3/baseline_dataset_exp_colab.R
+++ kaggle_comp3/baseline_dataset_exp_colab.R
@@ -17,6 +17,10 @@ dataset[, (lagcols1_names) :=  shift(.SD, 1), .SDcols = lagcols, by=numero_de_cl
 dataset[, (lagcols3_names) :=  shift(.SD, 3), .SDcols = lagcols, by=numero_de_cliente]
 dataset[, (lagcols6_names) :=  shift(.SD, 6), .SDcols = lagcols, by=numero_de_cliente]
 
+meses <- c(202012,seq(202101,202107,1))
+
+dataset <- dataset[foto_mes %in% meses,]
+
 
 fwrite(dataset,
        file = "buckets/b1/datasets/dataset_baseline_exp_colab.csv.gz",
