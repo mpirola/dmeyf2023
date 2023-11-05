25ef753594a2f318488e2ea54b32a1a58dc1f5a8
mpirola <malenapirola@gmail.com>
2023-11-05 13:25:53 +0000

    FIX

--- kaggle_comp3/823_BO_exp_colab_grupoB_bagging.r
+++ kaggle_comp3/823_BO_exp_colab_grupoB_bagging.r
@@ -54,7 +54,8 @@ PARAM$lgb_semilla <- 200177
 #------------------------------------------------------------------------------
 
 # Hiperparametros FIJOS de  lightgbm
-PARAM$lgb_basicos <- list(
+
+PARAM$finalmodel$lgb_basicos <- list(
   boosting = "gbdt", # puede ir  dart  , ni pruebe random_forest
   objective = "binary",
   metric = "custom",
@@ -63,37 +64,44 @@ PARAM$lgb_basicos <- list(
   feature_pre_filter = FALSE,
   force_row_wise = TRUE, # para reducir warnings
   verbosity = -100,
+
   min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
   min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
   lambda_l1 = 0.0, # lambda_l1 >= 0.0
   lambda_l2 = 0.0, # lambda_l2 >= 0.0
-  num_iterations = 9999, # un numero muy grande, lo limita early_stopping_rounds
+  max_bin = 31L, # lo debo dejar fijo, no participa de la BO
+  
   is_unbalance = FALSE, #
   scale_pos_weight = 1.0, # scale_pos_weight > 0.0
+  
   drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
   max_drop = 50, # <=0 means no limit
   skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
+  
   extra_trees = TRUE, # Magic Sauce
-  seed = PARAM$lgb_semilla
+  
+  seed = PARAM$finalmodel$semilla
 )
+
 
 
 # Aqui se cargan los hiperparametros que se optimizan
 #  en la Bayesian Optimization
 
 PARAM$bo_lgb <- makeParamSet(
-  makeDiscreteParam("data_sample_strategy", values = c("goss", "bagging")),
   makeNumericParam("learning_rate", lower = 0.02, upper = 0.3),
   makeNumericParam("feature_fraction", lower = 0.01, upper = 1.0),
   makeIntegerParam("num_leaves", lower = 8L, upper = 1024L),
   makeIntegerParam("min_data_in_leaf", lower = 100L, upper = 50000L),
+  #nuevos hp
   makeNumericParam("feature_fraction_bynode", lower = 0.01, upper = 1.0),
-  makeIntegerParam("max_bin", lower = 20L, upper = 2000L),
+
   makeIntegerParam("max_depth", lower = 2L, upper = 50L),
-  makeNumericParam("top_rate", lower = 0.01, upper = 0.5),
-  makeNumericParam("other_rate", lower = 0.01, upper = 0.5),
-  makeNumericParam("bagging_fraction", lower = 0.01, upper = 0.9),
-  makeIntegerParam("baggin_freq", lower = 1L, upper = 10L)
+
+  makeNumericParam("bagging_fraction", lower = 0.0, upper = 1.0),
+  makeNumericParam("pos_bagging_fraction", lower = 0.0, upper = 1.0),
+  makeNumericParam("neg_bagging_fraction", lower = 0.0, upper = 1.0),
+  makeIntegerParam("baggin_freq", lower = 1L, upper = 30L)
 )
 
 
