e0c4bffd51873762eefb2aabaa59761259043618
mpirola <malenapirola@gmail.com>
2023-11-06 01:20:23 +0000

    FIX: agrego meses adicionales

--- kaggle_comp3/baseline_dataset_exp_colab.R
+++ kaggle_comp3/baseline_dataset_exp_colab.R
@@ -17,7 +17,7 @@ dataset[, (lagcols1_names) :=  shift(.SD, 1), .SDcols = lagcols, by=numero_de_cl
 dataset[, (lagcols3_names) :=  shift(.SD, 3), .SDcols = lagcols, by=numero_de_cliente]
 dataset[, (lagcols6_names) :=  shift(.SD, 6), .SDcols = lagcols, by=numero_de_cliente]
 
-meses <- c(202012,seq(202101,202107,1))
+meses <- c(202010,seq(202101,202109,1))
 
 dataset <- dataset[foto_mes %in% meses,]
 
