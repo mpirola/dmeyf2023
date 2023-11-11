a8187e6b9ee75337e883c52fb620cf02252e6667
mpirola <malenapirola@gmail.com>
2023-11-11 23:39:11 +0000

    agrego test para numero optimo de envios de cada modelo

--- kaggle_comp3/exp_colab_comp_resultados.R
+++ kaggle_comp3/exp_colab_comp_resultados.R
@@ -7,11 +7,13 @@ bl <- read_delim("buckets/b1/exp/KA8240_baseline_exp_colab/KA8240_baseline_exp_c
 goss <- read_delim("buckets/b1/exp/KA8240_exp_colab_goss/KA8240_exp_colab_goss_ganancias_semillerio.csv")
 bagging <- read_delim("buckets/b1/exp/KA8240_exp_colab_bagging/KA8240_exp_colab_bagging_ganancias_semillerio.csv")
 
+bagging_opt <- bagging %>% filter(envios == envios[1])
+bl_opt <- bl %>% filter(envios == envios[1])
+goss_opt <- goss %>% filter(envios == envios[1])
 
-
-#test_bag_bl <- wilcox.test(bagging$ganancia,bl$ganancia, alternative = 'greater',paired = T)
-#test_goss_bl <- wilcox.test(goss$ganancia,bl$ganancia, alternative = 'greater',paired = T)
-#test_bag_goss <- wilcox.test(goss$ganancia,bagging$ganancia, alternative = 'two.sided',paired = T)
+test_bag_bl_opt <- wilcox.test(bagging_opt$ganancia,bl_opt$ganancia, alternative = 'greater',paired = T)
+test_goss_bl_opt <- wilcox.test(goss_opt$ganancia,bl_opt$ganancia, alternative = 'greater',paired = T)
+test_bag_goss_opt <- wilcox.test(goss_opt$ganancia,bagging_opt$ganancia, alternative = 'two.sided',paired = T)
 
 bl <- bl %>% 
   rename(baseline = ganancia)
@@ -62,10 +64,10 @@ graf_envios <- ggplot(tabla, aes(x = envios, y = ganancia,colour = modelo)) +
 graf_envios
 
 
-# tests <- tribble(~comparacion,~p.value,~statistic,
-#                  "bagging vs. baseline",test_bag_bl$p.value,test_bag_bl$statistic[1],
-#                  "goss vs. baseline",test_goss_bl$p.value,test_goss_bl$statistic[1],
-#                  "bagging vs. goss",test_bag_goss$p.value,test_bag_goss$statistic[1])
+tests <- tribble(~comparacion,~p.value,~statistic,
+                  "bagging vs. baseline",test_bag_bl_opt$p.value,test_bag_bl_opt$statistic[1],
+                  "goss vs. baseline",test_goss_bl_opt$p.value,test_goss_bl_opt$statistic[1],
+                  "bagging vs. goss",test_bag_goss_opt$p.value,test_bag_goss_opt$statistic[1])
 
 
 writexl::write_xlsx(list("summary" = tabla,
