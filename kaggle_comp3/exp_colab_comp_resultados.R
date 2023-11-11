b2551bb828174d2bbbc26b58b788cdd2686c21ef
mpirola <malenapirola@gmail.com>
2023-11-11 23:22:25 +0000

    armado de procesamiento para resultados exp colaborativos

--- kaggle_comp3/exp_colab_comp_resultados.R
+++ kaggle_comp3/exp_colab_comp_resultados.R
@@ -7,12 +7,14 @@ bl <- read_delim("buckets/b1/exp/KA8240_baseline_exp_colab/KA8240_baseline_exp_c
 goss <- read_delim("buckets/b1/exp/KA8240_exp_colab_goss/KA8240_exp_colab_goss_ganancias_semillerio.csv")
 bagging <- read_delim("buckets/b1/exp/KA8240_exp_colab_bagging/KA8240_exp_colab_bagging_ganancias_semillerio.csv")
 
-test_bag_bl <- wilcox.test(bagging$ganancia,bl$ganancia, alternative = 'greater',paired = T)
-test_goss_bl <- wilcox.test(goss$ganancia,bl$ganancia, alternative = 'greater',paired = T)
-test_bag_goss <- wilcox.test(goss$ganancia,bagging$ganancia, alternative = 'two.sided',paired = T)
+
+
+#test_bag_bl <- wilcox.test(bagging$ganancia,bl$ganancia, alternative = 'greater',paired = T)
+#test_goss_bl <- wilcox.test(goss$ganancia,bl$ganancia, alternative = 'greater',paired = T)
+#test_bag_goss <- wilcox.test(goss$ganancia,bagging$ganancia, alternative = 'two.sided',paired = T)
 
 bl <- bl %>% 
-  rename(bl = ganancia)
+  rename(baseline = ganancia)
 
 goss <- goss %>% 
   rename(goss = ganancia)
@@ -25,19 +27,45 @@ bagging <- bagging %>%
 tabla <- bl %>% 
   full_join(goss) %>% 
   full_join(bagging) %>% 
-  pivot_longer(cols = c(3:5)) %>% 
-  group_by(name) %>% 
-  summarize(media = mean(value/1000000),
-            sd = sd(value/1000000),
-            median = median(value/1000000)) %>% 
+  #filter(!is.na(baseline) & !is.na(bagging) & !is.na(goss)) %>% 
+  pivot_longer(cols = c(baseline,goss,bagging),
+               names_to = "modelo",
+               values_to = "ganancia") %>% 
+  mutate(ganancia = ganancia/1000000)
+
+summary <- tabla %>% 
+  group_by(modelo,envios) %>% 
+  summarize(media = mean(ganancia),
+            sd = sd(ganancia),
+            median = median(ganancia)) %>% 
   ungroup() %>% 
   distinct() %>% 
-  mutate(n = 20) 
+  mutate(n = 20)
 
-tests <- tribble(~comparacion,~p.value,~statistic,
-                 "bagging vs. baseline",test_bag_bl$p.value,test_bag_bl$statistic[1],
-                 "goss vs. baseline",test_goss_bl$p.value,test_goss_bl$statistic[1],
-                 "bagging vs. goss",test_bag_goss$p.value,test_bag_goss$statistic[1])
+#
+
+### Grafico curvas por envios
+
+graf_envios <- ggplot(tabla, aes(x = envios, y = ganancia,colour = modelo)) +
+  geom_smooth() +
+  geom_vline(xintercept = 12636,
+             colour = "#F8766D",
+             linetype = "dashed") +
+  geom_vline(xintercept = 13117,
+             colour = "#00BA38",
+             linetype = "dashed") +
+  geom_vline(xintercept = 11755,
+             colour = "#619CFF",
+             linetype = "dashed")
+  
+
+graf_envios
+
+
+# tests <- tribble(~comparacion,~p.value,~statistic,
+#                  "bagging vs. baseline",test_bag_bl$p.value,test_bag_bl$statistic[1],
+#                  "goss vs. baseline",test_goss_bl$p.value,test_goss_bl$statistic[1],
+#                  "bagging vs. goss",test_bag_goss$p.value,test_bag_goss$statistic[1])
 
 
 writexl::write_xlsx(list("summary" = tabla,
